c454bd953ea07042f459889a2e6d130cc8928ca1
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-02 19:49:45 +0800

    完成首页展示工作

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  enum status:{ user: 0, admin: 1 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
     resources :works
   end
 
-  root "welcome#index"
+  root "works#index"
 end
