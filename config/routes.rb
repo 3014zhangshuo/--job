04eb430ee2d954f9f80820d290acd134ac4f699d
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-04 11:05:46 +0800

    add view and routes to admin/user features

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+
 devise_for :users, :controllers => { :registrations => "registrations" }
 
   namespace :company do
     resources :works
   end
 
+  namespace :admin do
+    resource :user, only: [:index, :show, :destroy] do
+      member do
+        post :admin
+        post :user
+      end
+    end
+  end
+
   root "works#index"
+
 end
