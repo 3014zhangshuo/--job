87950488fafb1b4399964b3fc185faae75980ecb
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-04 10:46:16 +0800

    简易的登录导向功能

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  before_filter :configure_permitted_parameters
+
+ protected
+
+ def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:status])
+ end
+
+ def after_sign_up_path_for(resource)
+     if params[:status] = "admin"
+       company_works_path
+     elsif params[:status] = "user"
+      root_path
+     end
+   end
+
+end
