ca2065d25cb93e2f42af064f61c725a646b5c1e3
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-04 13:21:52 +0800

    create resume pdf

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,6 @@
 class Resume < ApplicationRecord
   belongs_to :user
   belongs_to :work
+  mount_uploader :attachment, AttachmentUploader
+  validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104050318) do
+ActiveRecord::Schema.define(version: 20170104051926) do
 
   create_table "resumes", force: :cascade do |t|
     t.integer  "user_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170104050318) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "attachment"
   end
 
   create_table "users", force: :cascade do |t|
