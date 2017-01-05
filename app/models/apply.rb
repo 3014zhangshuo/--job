fe9eb60cba4c68d53165662a1c2117495c1d9ac9
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-05 15:17:10 +0800

    add assm even to models/apply.rb

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -1,3 +1,25 @@
 class Apply < ApplicationRecord
   belongs_to :user
+
+  include AASM
+
+  aasm do
+    state :apply_sended, initial: true
+    state :apply_passing
+    state :apply_passed
+    state :apply_failed
+
+    event :passing do
+      transitions from: :apply_sended, to: :apply_passing
+    end
+
+    event :passed do
+      transitions from: :apply_passing, to: :apply_passed
+    end
+
+    event :failed do
+      transitions from: :apply_passing, to: :apply_failed
+    end
+  end
+
 end
