dcf3d09dc51e4794798fecd470087be0f0ee3ba4
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-06 10:31:17 +0800

    add show controller and get conversation

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,14 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
   before_action :get_mailbox
+  before_action :get_conversation, except: [:index]
+
 
   def index
     @conversations = @mailbox.inbox
+  end
+
+  def show
   end
 
   private
@@ -11,4 +16,9 @@ class ConversationsController < ApplicationController
     def get_mailbox
       @mailbox ||= current_user.mailbox
     end
+
+    def get_conversation
+      @conversation ||= @mailbox.conversations.find(params[:id])
+    end
+
 end
